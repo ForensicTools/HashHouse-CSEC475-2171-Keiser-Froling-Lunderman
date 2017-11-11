b1285f7aa5a423f3e7a8b780a498e94bf8d09c4b
Morgan Keiser <mrk1160@rit.edu>
2017-11-11 11:56:13 -0500

    Update hashFiles.py

--- hashFiles.py
+++ hashFiles.py
@@ -1,13 +1,31 @@
 # Name: hashFiles.py
 # Authors: Keiser, Froling, Lunderman
-# Last Revision: 11/03/2017
+# Last Revision: 11/11/2017
 # Desc: hash all files on an OS and output to a file that will be used with diff
  later
+ # tested on: <list os's that have been added to the test list>
 
 # imports
 import hashlib
 import os
 import sys
+import platform
+from subprocess import call
+
+# name: find_os
+# desc: figures out which os is running on the computer the script is running, to know to iterate through "/" or drives (C:, etc)
+# params: 
+# returns: windows or posix
+def find_os():
+        name = call(["os.name()"]) # gets the os name (for unix, "posix")
+        print ("NAME: %s", name)
+        if name == "posix":
+                return name
+        else:
+                name = call[("platform.system")]
+                print ("NAME2: %s", name)
+                if name == "Windows":
+                        return name
 
 # name: hashFile 
 # Desc: hashes a file 
@@ -28,6 +46,7 @@ def main():
                         pass
                 else:
                         outfile = raw_input("Enter an output file path: ")
+        name = find_os() # return Unix or Windows
         for root, directs, files in os.walk():
                 for f in files:
                 ret_hash = hashfile(f)
